aa6d210cc9835923c25aa3ec2ee52dc6ea09c051
danteBenitez <benitezdante123@gmail.com>
2024-05-14 13:55:28 -0300

    feat: Add class-validator-package and global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(
+    app.get('Reflector'),
+    {
+      excludePrefixes: ['_'],
+    }
+  ));
+
   await app.listen(3000);
 }
 bootstrap();
