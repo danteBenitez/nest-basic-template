577b96262b774cbb73245ead4abf0c506d08b634
danteBenitez <benitezdante123@gmail.com>
2024-05-17 12:25:14 -0300

    fix(users, auth): Correct  module imports and exports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Role } from './entities/role.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ENVIRONMENT } from '@/config/env';
+import { UsersModule } from '@/users/users.module';
 
 @Module({
+  imports: [
+    UsersModule,
+    TypeOrmModule.forFeature([Role]), 
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<ENVIRONMENT['SECRET']>('SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Role } from '@/auth/entities/role.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    ConfigModule,
+    TypeOrmModule.forFeature([User, Role]),
   ],
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService]
 })
 export class UsersModule {}
