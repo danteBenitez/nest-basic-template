f7b0a6243179bdf92d832ceeb2cff157a8bb79b5
danteBenitez <benitezdante123@gmail.com>
2024-08-09 14:49:57 -0300

    fix: Role guard not working as expected

--- src/auth/guards/role.guard.ts
+++ src/auth/guards/role.guard.ts
@@ -1,13 +1,17 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Request } from "express";
 import { ROLE_KEY } from "../decorators/role.decorator";
+import { Reflector } from "@nestjs/core";
 
+@Injectable()
 export class RoleGuard implements CanActivate {
+    constructor(private reflector: Reflector) {}
+
     canActivate(context: ExecutionContext): boolean {
         const request = context.switchToHttp().getRequest<Request>();
         const user = request.user;
         const roles = user?.roles;
-        const requiredRole = Reflect.getMetadata(ROLE_KEY, [
+        const requiredRole = this.reflector.getAllAndOverride(ROLE_KEY, [
             context.getHandler(),
             context.getClass(),
         ]);
