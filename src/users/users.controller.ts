d34eec50ab557701c8f38c57797b2aaa35dc9307
danteBenitez <benitezdante123@gmail.com>
2024-08-14 09:45:06 -0300

    feat: Query projects by author

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,35 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Role } from '@/auth/decorators/role.decorator';
 import { ROLES } from '@/auth/consts';
 import { RoleGuard } from '@/auth/guards/role.guard';
+import { ProjectsService } from '@/projects/projects.service';
+import { Request } from 'express';
+import { JwtAuthGuard } from '@/auth/guards/auth.guard';
 
 @Controller('/users')
+@UseGuards(JwtAuthGuard)
 @UseGuards(RoleGuard)
 export class UsersController {
     constructor(
-        private readonly usersService: UsersService
+        private readonly usersService: UsersService,
+        private readonly projectService: ProjectsService
     ) {}
 
     @Role(ROLES.ADMIN)
     @Get()
     async findAll() {
         return this.usersService.findAll();
+    }
+
+    @Get("/projects")
+    async findProjects(
+        @Req() req: Request
+    ) {
+        const user = req.user;
+        if (!user) {
+            throw new UnauthorizedException("Usuario no autenticado");
+        }
+        return this.projectService.findByAuthor(user);
     }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,13 +5,17 @@ import { User } from './entities/user.entity';
 import { Role } from '@/auth/entities/role.entity';
 import { ConfigModule } from '@nestjs/config';
 import { UsersController } from './users.controller';
+import { ProjectsModule } from '@/projects/projects.module';
+import { ProjectsService } from '@/projects/projects.service';
+import { Project } from '@/projects/entities/project.entity';
 
 @Module({
   imports: [
     ConfigModule,
-    TypeOrmModule.forFeature([User, Role]),
+    TypeOrmModule.forFeature([User, Role, Project]),
+    ProjectsModule,
   ],
-  providers: [UsersService],
+  providers: [UsersService, ProjectsService],
   exports: [UsersService],
   controllers: [UsersController]
 })
